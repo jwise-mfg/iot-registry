dfb3935a8b61aa7f0a690b5f7583267eb8681f46
Jonathan Wise <jonathan.wise@cesmii.org>
2023-09-25 10:14:06 -0400

    make more dynamic

--- scripts/checkin/index.php
+++ scripts/checkin/index.php
@@ -1,6 +1,7 @@
 <?php
 $script = file_get_contents('./checkin.sh', true);
-$script = str_replace("<SERVERPATH>", "https://thisisatest", $script);
+$script = str_replace("<SERVERPATH>", ("https://" . $_SERVER['HTTP_HOST']), $script);
 header('Content-type: text/plain; charset=utf-8');
+header('Content-Disposition: filename="checkin.sh"');
 echo $script;
 ?>
