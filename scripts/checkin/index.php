a980ebc829e1e5bceb2c33a5fd75df0fc7d8ed5e
Jonathan Wise <jonathan.wise@cesmii.org>
2023-09-25 10:10:41 -0400

    fix typo

--- scripts/checkin/index.php
+++ scripts/checkin/index.php
@@ -1,5 +1,5 @@
 <?php
-$script = file_get_contents('./check.sh', true);
+$script = file_get_contents('./checkin.sh', true);
 $script = str_replace("<SERVERPATH>", "https://thisisatest", $script);
 header('Content-type: text/plain; charset=utf-8');
 echo $script;
